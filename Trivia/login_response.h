475fcc0d84867c54defaf0674587357f9436cc07
Almog Hamdani <almogh52@gmail.com>
2019-05-06 11:42:59 +0300

    Implement serialize and deserialize for all req/res

--- Trivia/login_response.h
+++ Trivia/login_response.h
@@ -1,5 +1,16 @@
 #pragma once
 
+#include "json.hpp"
+using nlohmann::json;
+
 struct LoginResponse {
 	unsigned int status;
 };
+
+void to_json(json& j, const LoginResponse& res) {
+	j = json{ {"status", res.status} };
+}
+
+void from_json(json& j, LoginResponse& res) {
+	j.at("status").get_to(res.status);
+}
