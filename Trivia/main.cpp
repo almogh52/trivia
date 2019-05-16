6db2463065d4e36fe2928949dc297ec56a451bba
Almog Hamdani <almogh52@gmail.com>
2019-05-16 22:13:18 +0300

    Add missing WSA initialization

--- Trivia/main.cpp
+++ Trivia/main.cpp
@@ -2,17 +2,22 @@
 #include <iostream>
 
 #include "server.h"
+#include "WSAInitializer.h"
 
 int main()
 {
     std::unique_ptr<Server> server(new Server());
+    std::unique_ptr<WSAInitializer> wsaInitializer(new WSAInitializer());
 
     try {
 	// Try to run the server
 	server->run();
     }
+    catch (std::exception& ex) {
+	std::cout << ex.what() << std::endl;
+    }
     catch (...) {
-	std::cout << "An an error occurred running the server" << std::endl;
+	std::cout << "An an unknown error occurred running the server" << std::endl;
     }
 
     return 0;
