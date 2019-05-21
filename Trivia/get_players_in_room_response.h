80771ef94bce6a7b6ecbae505dc6c23b5270151a
Almog Hamdani <almogh52@gmail.com>
2019-05-21 10:43:36 +0300

    Implement get players in room response structure

---
+++ Trivia/get_players_in_room_response.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+
+#include "json.hpp"
+using nlohmann::json;
+
+#include "room_data.h"
+
+struct GetPlayersInRoomResponse {
+    std::vector<std::string> rooms;
+};
+
+void to_json(json& j, const GetPlayersInRoomResponse& res) {
+    j = json{ {"rooms", res.rooms} };
+}
+
+void from_json(const json& j, GetPlayersInRoomResponse& res) {
+    j.at("rooms").get_to(res.rooms);
+}
