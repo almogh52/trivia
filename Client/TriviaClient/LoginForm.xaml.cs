e253e4398a1cccee2e365db12f533fccdfb4fd46
Almog Hamdani <almogh52@gmail.com>
2019-06-11 10:45:01 +0300

    Implement the login backend

--- Client/TriviaClient/LoginForm.xaml.cs
+++ Client/TriviaClient/LoginForm.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +18,19 @@ using System.Windows.Shapes;
 
 namespace TriviaClient
 {
+    public struct LoginRequest
+    {
+        public const int CODE = 0;
+
+        public string username;
+        public string password;
+    }
+
+    public struct LoginResponse
+    {
+        public int status;
+    }
+
     /// <summary>
     /// Interaction logic for LoginForm.xaml
     /// </summary>
@@ -23,6 +39,48 @@ namespace TriviaClient
         public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private async void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameField.Text;
+            string password = passwordField.Password;
+
+            // Show error if the username or password is missing
+            if (username.Length == 0 || password.Length == 0)
+            {
+                await DialogHost.Show(new Dialogs.MessageDialog { Message = "One of the required fields is missing" });
+
+                return;
+            }
+
+            // Show the loading dialog
+            await DialogHost.Show(new Dialogs.LoadingDialog(), async delegate (object s, DialogOpenedEventArgs eventArgs)
+            {
+                LoginResponse res;
+                byte[] resBytes;
+
+                LoginRequest req = new LoginRequest
+                {
+                    username = username,
+                    password = password
+                };
+
+                // Send the login request
+                await Client.Send(LoginRequest.CODE, Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(req)));
+
+                // Get the login response
+                resBytes = await Client.Recv();
+
+                // Deserialize the lgin response
+                res = JsonConvert.DeserializeObject<LoginResponse>(Encoding.UTF8.GetString(resBytes));
+
+                // If login failed, show error
+                if (res.status == 1)
+                {
+                    eventArgs.Session.UpdateContent(new Dialogs.MessageDialog { Message = "Incorrect username and password entered!" });
+                }
+            }, null);
         }
     }
 }
