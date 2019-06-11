1acff0d100861f7d086029b3b2f2f192ffdaaf8b
Almog Hamdani <almogh52@gmail.com>
2019-06-11 19:29:39 +0300

    Implement a simple menu request handler

---
+++ Server/Trivia/menu_request_handler.cpp
@@ -0,0 +1,49 @@
+#include "menu_request_handler.h"
+
+#include "logout_response.h"
+#include "response_status.h"
+#include "json_response_packet_serializer.hpp"
+
+MenuRequestHandler::MenuRequestHandler(std::shared_ptr<LoggedUser> user, std::shared_ptr<RoomManager> roomManager, std::shared_ptr<LoginManager> loginManager, std::shared_ptr<RequestHandlerFactory> handlerFactory)
+    : m_user(user), m_roomManager(roomManager), m_loginManager(loginManager), m_handlerFactory(handlerFactory)
+{
+}
+
+bool MenuRequestHandler::isRequestRelevant(const Request & req) const
+{
+    return req.id == LOGOUT_REQUEST || req.id == GET_ROOMS_REQUEST || req.id == GET_PLAYERS_IN_ROOM_REQUEST || req.id == JOIN_ROOM_REQUEST || req.id == CREATE_ROOM_REQUEST;
+}
+
+RequestResult MenuRequestHandler::handleRequest(const Request & req) const
+{
+    RequestResult res;
+
+    switch (req.id)
+    {
+    case LOGOUT_REQUEST:
+	res = logout(req);
+    }
+}
+
+RequestResult MenuRequestHandler::logout(const Request & req) const
+{
+    LogoutResponse logoutRes;
+    RequestResult res;
+
+    // Try to logout the user
+    if (m_loginManager->logout(m_user->getUsername()))
+    {
+	logoutRes.status = SUCCESS;
+    }
+    else {
+	logoutRes.status = ERROR;
+    }
+
+    // Serialize the new packet and set the next handler
+    res.newHandler = nullptr;
+    res.response = JsonResponsePacketSerializer::SerializePacket(logoutRes);
+
+    return res;
+}
+
+
---
+++ Server/Trivia/menu_request_handler.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "request_handler_interface.h"
+
+#include "logged_user.h"
+#include "room_manager.h"
+#include "request_handler_factory.h"
+
+class MenuRequestHandler :
+    public IRequestHandler
+{
+public:
+    MenuRequestHandler(std::shared_ptr<LoggedUser> user, std::shared_ptr<RoomManager> roomManager, std::shared_ptr<LoginManager> loginManager, std::shared_ptr<RequestHandlerFactory> handlerFactory);
+    
+    virtual bool isRequestRelevant(const Request& req) const;
+    virtual RequestResult handleRequest(const Request& req) const;
+
+private:
+    RequestResult logout(const Request& req) const;
+
+    std::shared_ptr<LoggedUser> m_user;
+    std::shared_ptr<RoomManager> m_roomManager;
+    std::shared_ptr<LoginManager> m_loginManager;
+    std::shared_ptr<RequestHandlerFactory> m_handlerFactory;
+};
+
