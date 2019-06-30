572f5839c6e87cc373e51199a36e4a1cd3bedde8
Almog Hamdani <almogh52@gmail.com>
2019-06-30 13:46:15 +0300

    Implement the room admin window

---
+++ Client/TriviaClient/RoomAdminWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TriviaClient
+{
+    /// <summary>
+    /// Interaction logic for RoomWindow.xaml
+    /// </summary>
+    public partial class RoomAdminWindow : Window
+    {
+        public RoomData room { get; set; }
+
+        public struct GetRoomStateRequest
+        {
+            public const int CODE = 10;
+        }
+
+        public struct GetRoomStateResponse
+        {
+            public int status;
+            public bool hasGameBegun;
+            public List<string> players;
+            public int questionCount;
+            public int answerTimeout;
+        }
+
+        public struct CloseRoomRequest
+        {
+            public const int CODE = 8;
+        }
+
+        public struct CloseRoomResponse
+        {
+            public int status;
+        }
+
+        public RoomAdminWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Set title
+            Title = $"Trivia - ${room.name} - Room #${room.id}";
+
+            // Set the action handler of the room preview
+            roomPreview.Action = CloseRoom;
+
+            // Run the action in the background
+            Task.Run(new Action(RefreshRoomData));
+        }
+
+        private async void RefreshRoomData()
+        {
+            GetRoomStateResponse res;
+            byte[] buf;
+
+            while (true)
+            {
+                // Send the get room state request
+                await Client.Send(GetRoomStateRequest.CODE, new byte[0]);
+
+                // Get from the server the room state
+                buf = await Client.Recv();
+
+                // Deserialize the response
+                res = JsonConvert.DeserializeObject<GetRoomStateResponse>(Encoding.UTF8.GetString(buf));
+
+                // Set the room preview data
+                roomPreview.Data = new RoomPreviewData
+                {
+                    Id = room.id,
+                    Name = room.name,
+                    MaxPlayers = room.maxPlayers,
+                    TimePerQuestion = res.answerTimeout,
+                    QuestionCount = res.questionCount,
+                    IsActive = res.hasGameBegun,
+                    ActionButtonText = "Close Room",
+                    ActionButtonEnabled = true,
+                    Players = res.players
+                };
+
+                // Update the room preview in the main thread
+                roomPreview.Dispatcher.Invoke(new Action(() => { roomPreview.Update(); }));
+
+                // Wait 1 second
+                await Task.Delay(1000);
+            }
+        }
+
+        private async void CloseRoom(int roomId)
+        {
+            CloseRoomResponse res;
+            byte[] buf;
+
+            // Send the close room request
+            await Client.Send(CloseRoomRequest.CODE, new byte[0]);
+
+            // Get from the server the room state
+            buf = await Client.Recv();
+
+            // Deserialize the response
+            res = JsonConvert.DeserializeObject<CloseRoomResponse>(Encoding.UTF8.GetString(buf));
+
+            // Close this window and the rooms window
+            new RoomsWindow().Show();
+            Close();
+        }
+    }
+}
--- Client/TriviaClient/RoomsWindow.xaml.cs
+++ Client/TriviaClient/RoomsWindow.xaml.cs
@@ -117,7 +117,7 @@ namespace TriviaClient
         {
             InitializeComponent();
 
-            roomPreview.JoinRoom = JoinRoom;
+            roomPreview.Action = JoinRoom;
         }
 
         private async void JoinRoom(int roomId)
@@ -262,24 +262,24 @@ namespace TriviaClient
                     // Set the current dialog as an error dialog
                     eventArgs.Session.UpdateContent(new Dialogs.MessageDialog { Message = "An error occurred during creating the room!\nPlease try again later." });
                     return;
+                } else
+                {
+                    RoomData room = new RoomData
+                    {
+                        id = createRoomResponse.roomId,
+                        name = viewModel.Name,
+                        maxPlayers = viewModel.MaxPlayers,
+                        questionCount = viewModel.QuestionCount,
+                        timePerQuestion = viewModel.TimePerQuestion
+                    };
+
+                    // Show the room admin window
+                    new RoomAdminWindow { room = room }.Show();
+
+                    // Close this window
+                    Close();
                 }
             });
-
-            // If no error occurred, add the room to the list of rooms
-            if (createRoomResponse.status == 0)
-            {
-                Rooms.Add(new RoomData
-                {
-                    id = createRoomResponse.roomId,
-                    name = viewModel.Name,
-                    maxPlayers = viewModel.MaxPlayers,
-                    timePerQuestion = viewModel.TimePerQuestion,
-                    questionCount = viewModel.QuestionCount
-                });
-
-                // Re-set the list to apply changes
-                roomsList.ItemsSource = Rooms;
-            }
         }
 
         private async void refreshButton_Click(object sender, RoutedEventArgs e)
@@ -313,6 +313,10 @@ namespace TriviaClient
         {
             if (roomsList.SelectedItem == null)
             {
+                // Set an empty data in the room preview
+                roomPreview.Data = new RoomPreviewData();
+                roomPreview.Update();
+
                 return;
             }
 
@@ -324,7 +328,8 @@ namespace TriviaClient
                 MaxPlayers = roomData.maxPlayers,
                 TimePerQuestion = roomData.timePerQuestion,
                 QuestionCount = roomData.questionCount,
-                IsActive = roomData.isActive
+                IsActive = roomData.isActive,
+                ActionButtonText = "Join Room"
             };
 
             byte[] buf;
@@ -349,7 +354,7 @@ namespace TriviaClient
                 previewData.Players = res.players;
 
                 // Check if the room is joinable
-                previewData.IsJoinable = (previewData.IsActive && res.players.Count < previewData.MaxPlayers);
+                previewData.ActionButtonEnabled = (previewData.IsActive && res.players.Count < previewData.MaxPlayers);
 
                 // Set the new data of the room preview
                 roomPreview.Data = previewData;
