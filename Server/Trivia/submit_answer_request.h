f78feb7b7c330c9fb97a6084248a4b334d48e9a5
Almog Hamdani <almogh52@gmail.com>
2019-07-01 16:29:43 +0300

    Fix wrong name of request struct

--- Server/Trivia/submit_answer_request.h
+++ Server/Trivia/submit_answer_request.h
@@ -4,14 +4,14 @@
 #include "json.hpp"
 using nlohmann::json;
 
-struct CreateRoomRequest {
+struct SubmitAnswerRequest {
 	unsigned int answerId;
 };
 
-inline void to_json(json& j, const CreateRoomRequest& req) {
+inline void to_json(json& j, const SubmitAnswerRequest& req) {
 	j = json{ {"answerId", req.answerId} };
 }
 
-inline void from_json(const json& j, CreateRoomRequest& req) {
+inline void from_json(const json& j, SubmitAnswerRequest& req) {
 	j.at("answerId").get_to(req.answerId);
 }
